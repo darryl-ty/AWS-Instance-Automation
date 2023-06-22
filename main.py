f8da7dd4238d44d494eb08bb8ebd2466cf77d491
darryl-ty <darrylterrell921@gmail.com>
2023-06-21 20:17:25 -0400

    Start setting up jobs to handle the scheduled startup and shutdown of AWS instances.

--- main.py
+++ main.py
@@ -3,8 +3,36 @@ import schedule
 import boto3
 
 
+def aws_instance_shutdown():
+    pass
+
+
+def aws_instance_startup():
+    pass
+
+
+def job_scheduling():
+    schedule.every().monday.at("09:00", "US/Eastern").do(aws_instance_startup())
+    schedule.every().monday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+
+    schedule.every().tuesday.at("09:00", "US/Eastern").do(aws_instance_startup())
+    schedule.every().tuesday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+
+    schedule.every().wednesday.at("09:00", "US/Eastern").do(aws_instance_startup())
+    schedule.every().wednesday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+
+    schedule.every().thursday.at("09:00", "US/Eastern").do(aws_instance_startup())
+    schedule.every().thursday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+
+    schedule.every().friday.at("09:00", "US/Eastern").do(aws_instance_startup())
+    schedule.every().friday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 def main():
-    print("Hello World!")
+    job_scheduling()
 
 
 if __name__ == '__main__':
