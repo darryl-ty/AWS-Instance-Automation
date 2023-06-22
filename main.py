0a9c3951295f2c993c3b9abc36ac7faa8bd3f445
darryl-ty <darrylterrell921@gmail.com>
2023-06-21 20:21:15 -0400

    Import timezone library and fix typo in job_scheduling function.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pytz
 import time
 import schedule
 import boto3
@@ -12,24 +13,25 @@ def aws_instance_startup():
 
 
 def job_scheduling():
-    schedule.every().monday.at("09:00", "US/Eastern").do(aws_instance_startup())
-    schedule.every().monday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+    schedule.every().monday.at("09:00", "US/Eastern").do(aws_instance_startup)
+    schedule.every().monday.at("17:00", "US/Eastern").do(aws_instance_shutdown)
 
-    schedule.every().tuesday.at("09:00", "US/Eastern").do(aws_instance_startup())
-    schedule.every().tuesday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+    schedule.every().tuesday.at("09:00", "US/Eastern").do(aws_instance_startup)
+    schedule.every().tuesday.at("17:00", "US/Eastern").do(aws_instance_shutdown)
 
-    schedule.every().wednesday.at("09:00", "US/Eastern").do(aws_instance_startup())
-    schedule.every().wednesday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+    schedule.every().wednesday.at("09:00", "US/Eastern").do(aws_instance_startup)
+    schedule.every().wednesday.at("17:00", "US/Eastern").do(aws_instance_shutdown)
 
-    schedule.every().thursday.at("09:00", "US/Eastern").do(aws_instance_startup())
-    schedule.every().thursday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+    schedule.every().thursday.at("09:00", "US/Eastern").do(aws_instance_startup)
+    schedule.every().thursday.at("17:00", "US/Eastern").do(aws_instance_shutdown)
 
-    schedule.every().friday.at("09:00", "US/Eastern").do(aws_instance_startup())
-    schedule.every().friday.at("17:00", "US/Eastern").do(aws_instance_shutdown())
+    schedule.every().friday.at("09:00", "US/Eastern").do(aws_instance_startup)
+    schedule.every().friday.at("17:00", "US/Eastern").do(aws_instance_shutdown)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
+
 
 def main():
     job_scheduling()
